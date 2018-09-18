decd2a4c2dbd3ff313e28ca9cf3af85179c4b54a
Ryan Byrd <38544246+rdbyrd@users.noreply.github.com>
2018-09-18 19:02:28 -0400

    Add files via upload

---
+++ Lab04/loadJSONDoc.js
@@ -0,0 +1,16 @@
+/* 
+ * The function defined in this file loads a local JSON file
+ * and returns the string representation of the JSON object defined in the document.
+ */
+
+function loadJSON(file) {
+    //create an XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("application/json");
+    //define an synchronous request
+    xhr.open("GET", file, false);
+    //send the request
+    xhr.send();
+    //return the response
+    return xhr.responseText;
+}
